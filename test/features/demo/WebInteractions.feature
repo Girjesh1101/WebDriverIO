236a36cbb895cf15d3f545a2303719328ea74967
girjesh <girjeshgupta1101@gmail.com>
2026-02-16 10:32:36 +0530

    add input handling

---
+++ test/features/demo/WebInteractions.feature
@@ -0,0 +1,10 @@
+Feature: Web Interactions 
+
+
+    Scenario Outline: Demo Web Interaction
+        Given A web page is opened
+        When perform web interactions
+       
+       Examples:
+           | TestID | 
+           | ValuWeb_TC1  |
--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -15,3 +15,21 @@ When(/^the user clicks on make appointment button$/ , async function(){
 Then(/^the login page should be displayed$/, async function(){
     await expect(browser).toHaveUrl("https://katalon-demo-cura.herokuapp.com/profile.php#login");
 })
+
+// Web Interactions Steps
+
+Given(/^A web page is opened$/, async function () {
+    await browser.url('/inputs');
+    await browser.setTimeout({implicit: 15000, pageLoad:10000});
+    await browser.maximizeWindow();
+    
+})
+
+When(/^perform web interactions$/, async function(){
+
+    let ele = await $("[type='number']");
+    await ele.setValue("123456"); // clear field first then add value
+    // await ele.addValue("789"); // add value to existing value
+    await browser.pause(2000);
+
+});
